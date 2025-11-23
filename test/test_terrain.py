84ea20a0c26320d759061843128bbb6353f1fdfc
essaid <rehab.essaid@univ-rennes.fr>
2025-11-23 19:19:13 +0100

    modification de test_terrain

--- test/test_terrain.py
+++ test/test_terrain.py
@@ -8,7 +8,27 @@ class TestTerrain(unittest.TestCase):
 
     def test_chargement(self):
         # TODO
-        self.fail()
+        #fichier de test représente le terrain
+        contenu = """E~ /n
+        ~C~/n
+         ~~~/n"""
+        with open("terrain_test.txt", "w") as f:
+           f.write(contenu)
+        #chargement du fichier
+        t = Terrain()
+        t.charger("terrain_test.txt")
+        #vérification
+        self.assertEqual(t.largeur, 3)
+        self.assertEqual(t.hauteur, 3)
+        self.assertEqual(t[0][0], Case.ENTREE)
+        self.assertEqual(t[0][1], Case.VIDE)
+        self.assertEqual(t[0][2], Case.OBSTACLE)
+        self.assertEqual(t[1][0], Case.VIDE)
+        self.assertEqual(t[1][1], Case.CLIENT)
+        self.assertEqual(t[1][2], Case.VIDE)
+        self.assertEqual(t[2][0], Case.VIDE)
+        self.assertEqual(t[2][1], Case.VIDE)
+        self.assertEqual(t[2][2], Case.VIDE)
 
     def test_accesseur(self):
         t = Terrain()
